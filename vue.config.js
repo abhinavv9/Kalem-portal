60afc4c2323bb32878d13e374ec7a6e3648f3011
abhinavv9 <abhinavvrm14@gmail.com>
2023-07-24 17:10:06 +0530

    trying to fix

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@
 module.exports = {
     devServer: {
       proxy: {
-        '/api': {
-          target: 'http://54.80.111.38:8005', 
+        '/login': {
+          target: 'http://54.80.111.38:8005/api.login', 
           changeOrigin: true,
           secure: false,
           pathRewrite: {
