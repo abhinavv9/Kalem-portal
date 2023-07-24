ba171337fbb1e6a45030d91e0b2d2d0e5a0a1b0f
abhinavv9 <abhinavvrm14@gmail.com>
2023-07-24 17:24:14 +0530

    fix-2.5

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
-const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
 export default axios.create({
-  baseURL: corsProxyUrl + import.meta.env.VITE_APP_BACKEND_URL,
+  baseURL: import.meta.env.VITE_APP_BACKEND_URL,
   timeout: 15000,
   headers: {
     Accept: 'application/json',
